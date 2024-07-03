9050bc31bf7edcdb4cde3bf1305325d9080c47d6
Laryssa-lfa <lfadarkangel@gmail.com>
2024-07-03 18:05:01 -0300

    Refactor: Adjustments to date and text validations and addition of the airport list

--- flight_company.rb
+++ flight_company.rb
@@ -2,53 +2,75 @@ require 'date'
 
 origin_airport = ''; destination_airport = ''; departure_time = ''; arrival_time = ''
 response_type = ['S', 's', 'sim', 'Sim', 'SIM']
+airports = File.read('airports.json')
 
 puts '==========================================='
 puts '   Projeto de Companhia Aérea - Rebase'
-puts ''
 
 while (origin_airport.empty?) do
-  puts '- Qual é o aeroporto de origem?'
-  origin = gets.chomp
-  origin.match?(/^[[:alpha:]]+$/) ? (origin_airport = origin) : (puts 'Na resposta deve conter apenas letras.')
   puts ''
+  puts '- Qual é o aeroporto de origem? [Formato: abreviado com 3 letras, por exemplo: GRU]'
+  origin = gets.chomp
+  if (origin.length.eql?(3) && origin.match?(/^[[:alpha:]]+$/))
+    airports.include?(origin.upcase) ? (origin_airport = origin) : (puts '=> Aeroporto inválido.')
+  else
+    puts '=> Na resposta deve conter apenas 3 letras sem caracteres especiais.'
+  end
 end
 
 while (destination_airport.empty?) do
-  puts '- Qual é o aeroporto de destino?'
-  destination = gets.chomp
-  destination.match?(/^[[:alpha:]]+$/) ? (destination_airport = destination) : (puts 'Na resposta deve conter apenas.')
   puts ''
+  puts '- Qual é o aeroporto de destino? [Formato: abreviado com 3 letras, por exemplo: GRU]'
+  destination = gets.chomp
+  if (destination.length.eql?(3) && destination.match?(/^[[:alpha:]]+$/))
+    if (!destination.eql?(origin_airport))
+      airports.include?(destination.upcase) ? (destination_airport = destination) : (puts '=> Aeroporto inválido.')
+    else
+      puts '=> Aeroporto de destino não pode ser o mesmo de origem.'
+    end
+  else
+    puts '=> Na resposta deve conter apenas 3 letras sem caracteres especiais.'
+  end
 end
 
 while (departure_time.empty?) do
+  puts ''
   puts '- Qual a data que deseja partir? [Formato: dd/mm/aaaa]'
   date = gets.chomp
   if date.match?(/^\d{2}\/\d{2}\/\d{4}$/)
-    Date.parse(date) >= Date.today ? (departure_time = date) : (puts 'A data deve ser maior que hoje.')
+    begin
+      Date.parse(date) >= Date.today ? (departure_time = date) : (puts '=> A data deve ser maior que hoje.')
+    rescue
+      puts '=> Insira uma data válida.'
+    end
   else
-    puts 'Insira uma data válida no formato dd/mm/aaa.'
+    puts '=> Insira uma data válida e no formato dd/mm/aaaa.'
   end
-  puts ''
 end
 
+puts ''
 puts '- Deseja informar a data de retorno? S/N'
 arrival = gets.chomp
 
 while (arrival_time.empty?) do
+  puts ''
   puts '- Qual será a data de retorno? [Formato: dd/mm/aaaa]'
   date = gets.chomp
   if date.match?(/^\d{2}\/\d{2}\/\d{4}$/)
-    Date.parse(date) >= Date.today ? (arrival_time = date) : (puts 'A data deve ser maior que hoje.')
+    begin
+      date > departure_time ? (arrival_time = date) : (puts '=> A data deve ser maior que o dia da partida.')
+    rescue
+      puts '=> Insira uma data válida.'
+    end
   else
-    puts 'Insira uma data válida no formato dd/mm/aaa.'
+    puts '=> Insira uma data válida e no formato dd/mm/aaaa.'
   end
-  puts ''
 end if response_type.include?(arrival)
 
+puts ''
 puts '----------------------------'
 puts '   Resumo'
-puts " Aeroporto de origem: #{origin_airport}"
-puts " Aeroporto de destino: #{destination_airport}"
+puts " Aeroporto de origem: #{origin_airport.upcase}"
+puts " Aeroporto de destino: #{destination_airport.upcase}"
 puts " Data de partida: #{departure_time}"
 puts " Data de retorno: #{arrival_time.empty? ? 'sem data' : arrival_time }"
