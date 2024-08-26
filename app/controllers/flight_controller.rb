e7361d853c4cc426e6c0533566d75e7678216923
Laryssa-lfa <lfadarkangel@gmail.com>
2024-08-26 11:24:10 -0300

    Refactor: Separating the setup from the project

--- app/controllers/flight_controller.rb
+++ app/controllers/flight_controller.rb
@@ -1,21 +1,4 @@
 # frozen_string_literal: true
 
 class FlightController < ApplicationController
-  def search
-    flights = SearchFlightService.execute(permitted_params)
-
-    render json: flights, each_serializer: FlightSerializer
-  end
-
-  private
-
-  def permitted_params
-    params.permit(
-      :origin_airport,
-      :destination_airport,
-      :departure_time,
-      :arrival_time,
-      :fare_category
-    )
-  end
 end
