7861b8006a6ce12674202413bf9bbb0b24382e82
Laryssa-lfa <lfadarkangel@gmail.com>
2024-09-05 13:09:42 -0300

    Revert "Refactor: Rubocop"
    This reverts commit 949d200cb3a5170f3eab8571214961a0937a7bfe.

--- spec/helpers/stub_helper.rb
+++ spec/helpers/stub_helper.rb
@@ -5,9 +5,9 @@ module StubHelper
     stub_request(:get, url)
       .with(
         headers: {
-          Accept: '*/*',
+          'Accept': '*/*',
           'Accept-Encoding': 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          Host: ENV.fetch('HOST_API'),
+          'Host': ENV.fetch('HOST_API'),
           'User-Agent': 'Ruby',
           'X-Rapidapi-Host': ENV.fetch('HOST_API'),
           'X-Rapidapi-Key': ENV.fetch('KEY_API')
