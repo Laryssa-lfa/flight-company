90aa2cf17ed12d8d913925443f38bd222bfa253f
Laryssa-lfa <lfadarkangel@gmail.com>
2024-09-17 11:27:26 -0300

    Feat: Build search destination endpoint

---
+++ app/models/destination.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Destination < ApplicationRecord
+  scope :find_last_destinations, -> {
+    all_destinations_two_weeks = where(created_at: start_date..end_date).pluck(:name).tally
+    all_destinations_two_weeks.sort_by { |_key, value| -value }.first(5).to_h
+  }
+
+  def self.start_date
+    Time.zone.parse(-2.weeks.from_now.strftime('%d-%m-%Y'))
+  end
+
+  def self.end_date
+    Time.zone.parse(DateTime.current.strftime('%d-%m-%Y')).end_of_day
+  end
+end
